d7963429ce4750bc9b3b3b74fd074b21124bb478
gsingh62 <gsingh62@gatech.edu>
2020-11-21 19:56:17 -0800

    Updates to use generated BradleyInterface class from the proto file to send GameData to file

---
+++ src/output/SendGameData.kt
@@ -0,0 +1,49 @@
+package output
+
+import com.galexey.bradley.BradleyInterface.Coordinate
+import com.galexey.bradley.BradleyInterface.GameData
+import com.galexey.bradley.BradleyInterface.Node
+import com.galexey.bradley.BradleyInterface.NodeType
+import com.galexey.bradley.BradleyInterface.TurnData
+import map.ExitNode
+import map.OpenSpaceNode
+import map.WallNode
+import map.WorldMap
+import java.io.File
+import java.io.FileOutputStream
+
+class SendGameData {
+    fun sendGameData(worldMap: WorldMap, radius: Int, turns: List<map.Coordinate>) {
+        val gameData = GameData.newBuilder()
+        gameData.visibilityRadius = radius
+        worldMap.getAllCoordinates().forEach {
+            val coordinate = Coordinate.newBuilder()
+                    .setX(it.x)
+                    .setY(it.y)
+
+            val nodeType = when (worldMap.getNode(it)) {
+                is WallNode -> NodeType.WALL_NODE
+                is ExitNode -> NodeType.EXIT_NODE
+                is OpenSpaceNode -> NodeType.OPEN_NODE
+                else -> throw IllegalStateException("Node type can only be wall node, open space node or exit node")
+            }
+            val node = Node.newBuilder()
+                    .setNodeType(nodeType)
+                    .setCoordinate(coordinate)
+
+            gameData.addMap(node)
+        }
+        turns.forEach {
+            gameData.addTurns(TurnData.newBuilder()
+                    .setHero(Coordinate.newBuilder()
+                            .setX(it.x)
+                            .setY(it.y)
+                    )
+            )
+        }
+        val output = FileOutputStream(File("/Users/gayatrisingh/Bradley/src/output/game_data.txt"))
+        // gameData.build().writeTo(output)
+        output.write(gameData.toString().toByteArray())
+        output.close()
+    }
+}
